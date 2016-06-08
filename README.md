09efd7e6ae60f2a4653fd4da2c19af239060fe20
thisisaaronland <whosonfirst@mapzen.com>
2016-06-08 00:09:27 +0000

    update LICENSE

---
+++ README.md
@@ -0,0 +1,39 @@
+# py-mapzen-whosonfirst-uri
+
+Shared utilities for working with URIs for Who's On First documents
+
+## IMPORTANT
+
+This library is provided as-is, right now. It lacks proper
+documentation which will probably make it hard for you to use unless
+you are willing to poke and around and investigate things on your
+own.
+
+## Installation
+
+The usual Python dance:
+
+```
+sudo python setup.py install
+```    
+
+## Usage
+
+```
+import mapzen.whosonfirst.uri
+
+mapzen.whosonfirst.uri.id2fname(2179537)
+'2179537.geojson'
+
+mapzen.whosonfirst.uri.id2relpath(2179537)
+'217/953/7/2179537.geojson'
+
+mapzen.whosonfirst.uri.id2abspath('https://whosonfirst.mapzen.com/data', 2179537)
+'https://whosonfirst.mapzen.com/data/217/953/7/2179537.geojson'
+```
+
+## See also
+
+* https://github.com/whosonfirst/whosonfirst-data/
+
+
---
+++ mapzen/whosonfirst/uri/__init__.py
@@ -0,0 +1,52 @@
+# https://pythonhosted.org/setuptools/setuptools.html#namespace-packages
+__import__('pkg_resources').declare_namespace(__name__)
+
+import logging
+import os
+
+def id2abspath(root, id, **kwargs):
+
+    rel = id2relpath(id, **kwargs)
+
+    path = os.path.join(root, rel)
+    return path
+
+def id2relpath(id, **kwargs):
+
+    fname = id2fname(id, **kwargs)
+    parent = id2path(id)
+
+    path = os.path.join(parent, fname)
+    return path
+
+def id2fname(id, **kwargs):
+
+    # See this. It doesn't really allow for new "alternate" names
+    # to be added. For example a bespoke reverse geocoding polygon.
+    # That's not ideal but it's also by design to force us to
+    # actually think about what/how we want to do that...
+    # (20151217/thisisaaronland)
+
+    alt = kwargs.get('alt', None)
+    display = kwargs.get('display', None)
+
+    if alt:
+        return "%s-alt-%s.geojson" % (id, alt)
+    elif display:
+        return "%s-display-%s.geojson" % (id, display)
+    else:
+        return "%s.geojson" % id
+
+def id2path(id):
+
+    tmp = str(id)
+    parts = []
+    
+    while len(tmp) > 3:
+        parts.append(tmp[0:3])
+        tmp = tmp[3:]
+
+    if len(tmp):
+        parts.append(tmp)
+
+    return "/".join(parts)
