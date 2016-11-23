7eddc260f721cddd6c50c017fda4aded4e1c3597
Stephen Epps <stephen.epps@mapzen.com>
2016-11-22 17:06:30 -0800

    id2fname updates

--- mapzen/whosonfirst/uri/__init__.py
+++ mapzen/whosonfirst/uri/__init__.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import types
+import mapzen.whosonfirst.sources
 
 def id2abspath(root, id, **kwargs):
 
@@ -18,21 +20,71 @@ def id2relpath(id, **kwargs):
 
 def id2fname(id, **kwargs):
 
-    # See this. It doesn't really allow for new "alternate" names
-    # to be added. For example a bespoke reverse geocoding polygon.
-    # That's not ideal but it's also by design to force us to
-    # actually think about what/how we want to do that...
-    # (20151217/thisisaaronland)
-
+	# alt-geometry documentation: https://github.com/whosonfirst/whosonfirst-cookbook/blob/master/how_to/creating_alt_geometries.md
     alt = kwargs.get('alt', None)
-    display = kwargs.get('display', None)
+    source = kwargs.get('source', None)
+    function = kwargs.get('function', None)
+	extras = kwargs.get('extras', [])	
+	strict = kwargs.get('strict', False)
+	
+    display = kwargs.get('display', None)	# deprecated
 
-    if alt:
-        return "%s-alt-%s.geojson" % (id, alt)
-    elif display:
-        return "%s-display-%s.geojson" % (id, display)
-    else:
-        return "%s.geojson" % id
+	if type(alt) == types.StrType:
+		logging.warning("please stop passing alt as a string!")
+		source = alt
+		alt = True
+		
+	if display:
+		logging.warning("please stop passing display as a kwarg!")
+		function = display
+	
+	parts = [ id ]
+	
+	if alt and source:
+	
+		# this code does not exist yet
+		
+		"""
+		src = mapzen.whosonfirst.source.source(source)
+		
+		if src:
+			if function and !src.is_valid_function(function):
+				logging.warning("%s is not a valid function for source %s" % (function, source))
+		else:
+			logging.warning("%s is not a valid source" % source)
+		"""
+			
+		if not mapzen.whosonfirst.sources.is_valid_source(source):
+			logging.warning("%s is not a valid (or known) source" % source)
+	
+			if strict:
+				raise Exception, "invalid filename options"
+						
+	if alt and source:
+		# TO DO: test source against mapzen.whosonfirst.sources.is_valid_source
+		# TO DO: test function against... something?
+		# TO DO: if either scenario fails then... what? more than a warning?
+		
+		parts.append("alt")
+		parts.append(source)
+		
+		if function:
+			parts.append(function)			
+			parts.extend(extras)
+			
+	elif alt:
+		logging.warning("insufficient parameters for alt name")
+		
+		if strict:
+			raise Exception, "Invalid filename options"
+			
+	else:
+		pass
+		
+	fname = "-".join(parts)
+	fname = fname + ".geojson"
+
+	return fname
 
 def id2path(id):
 
